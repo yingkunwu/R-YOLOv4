2a948409d19648b2153a6087c07f79a5b9f163ab
kunnnnethan <ethanwu1127@gmail.com>
2021-10-10 00:14:01 +0800

    fix: :bug:

--- detect.py
+++ detect.py
@@ -30,7 +30,7 @@ class Detect:
         else:
             assert False, "Model is not exist"
         pretrained_dict = torch.load(weight_path, map_location=self.device)
-        self.model = Yolo(n_classes=self.args.number_of_classess)
+        self.model = Yolo(n_classes=self.args.number_of_classes)
         self.model = self.model.to(self.device)
         self.model.load_state_dict(pretrained_dict)
 
--- setup/setup.sh
+++ setup/setup.sh
@@ -2,9 +2,10 @@ wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=13LXboG6W7kXWkN7yTeMZ8PKzwcSUZJR2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=13LXboG6W7kXWkN7yTeMZ8PKzwcSUZJR2" -O UCAS_AOD.zip && rm -rf /tmp/cookies.txt
 unzip UCAS_AOD.zip
 rm UCAS_AOD.zip
+rm -r __MACOSX/
 mkdir weights
 cd weights
 mkdir pretrained
 cd ..
 mv yolov4.pth weights/pretrained
-mv -r UCAS_AOD weights
+mv UCAS_AOD weights
