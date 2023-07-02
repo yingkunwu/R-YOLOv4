62b7afb827c62b5b2ba8b80cc0b6da4d7713c6eb
eddie0509tw <eddie880509@gmail.com>
2023-07-03 00:50:53 +0800

    update tensorboard

--- train.py
+++ train.py
@@ -90,13 +90,23 @@ class Train:
         with open(os.path.join(self.model_path, 'opt.json'), 'w') as f:
             json.dump(to_save, f, indent=2)
     
-    def logging_processes(self, loss_items, global_step)->dict:
+    def logging_processes(self, loss_items, epoch,mean_recall = None,mean_precision = None ,map50 = None, map5095 = None, val_mode = False)->None:
         tensorboard_log = {}
+        # update in validation 
+        if val_mode and map50 != None and map5095 != None:
+            for name, metric in loss_items.items():
+                tensorboard_log[f"val/{name}"] = metric
 
-        for name, metric in loss_items.items():
-            tensorboard_log[f"train/{name}"] = metric
+            tensorboard_log["mean recall"] = mean_recall
+            tensorboard_log["mean precision"] = mean_precision
+            tensorboard_log["mAP@.5"] = map50
+            tensorboard_log["mAP@.5:.95"] = map5095
+        # update in training 
+        else:
+            for name, metric in loss_items.items():
+                tensorboard_log[f"train/{name}"] = metric
 
-        self.logger.list_of_scalars_summary(tensorboard_log, global_step)
+        self.logger.list_of_scalars_summary(tensorboard_log, epoch)
 
     def train(self):
         init()
@@ -159,8 +169,6 @@ class Train:
                     optimizer.step()
                     optimizer.zero_grad()
                     scheduler.step()
-                    
-                self.logging_processes(total_loss, loss_items, epoch, global_step, total_step, start_time)
                 
                 s = ('%10s'  + '%10.4g' * 6) % (
                     '%g/%g' % (epoch, self.args.epochs),scheduler.get_last_lr()[0],  loss_items["reg_loss"],
@@ -168,13 +176,15 @@ class Train:
 
                 pbar.set_description(s)
                 pbar.update(0)
+            # update the training log for tensorboard every epoch    
+            self.logging_processes(loss_items, epoch)
 
             # -------------------
             # ------ Valid ------
             # -------------------
             mp, mr, map50, map, loss, loss_items = test(self.model, self.device, self.class_names, self.args.data_folder, 
-                                self.args.dataset, self.args.img_size, self.args.batch_size * 2, conf_thres=0.001, nms_thres=0.65,epoch = epoch, val_mode = True)
-            
+                                self.args.dataset, self.args.img_size, self.args.batch_size * 2, conf_thres=0.001, nms_thres=0.65)
+            self.logging_processes(loss_items, epoch, mean_recall = mr, mean_precision = mp ,map50 = map50, map5095 = map, val_mode = True)
 
             fit = fitness(np.array([mp, mr, map50, map]))
             if fit > best_fitness:
